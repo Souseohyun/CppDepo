137fa1f0ff423750a148c7fe29731087755e18c6
Souseohyun <112543720+Souseohyun@users.noreply.github.com>
2023-02-15 16:04:50 +0800

    更新了

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -2,7 +2,23 @@
 #include<array>
 #include<vector>
 
+void myScanf(char endChar) {
+	char c{};
+	while ((c = std::cin.get()) != endChar) {
+		if (c >= '0' && c <= '9') {}
+		else if (c >= 'a' && c <= 'z') {
+			std::cout << char(c - 32);
+		}
+		else if (c >= 'A' && c <= 'Z') {
+			std::cout << char(c + 32);
+		}
+	}
+}
+
 int main() {
-	std::cout << "hello world" << std::endl;
+	
+	myScanf('@');
+
+	system("pause");
 	return 0;
 }
